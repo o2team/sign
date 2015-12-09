a9dfdde99deb8e87390ef79cbcecb6d40c56a3e3
HuiminLiu <hihuimin@gmail.com>
2015-12-09 11:12:21 +0800

    favicon

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.engine('.html',ejs.__express);
 app.set('view engine', 'html'); 
 
 // uncomment after placing your favicon in /public
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
