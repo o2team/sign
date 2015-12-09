3a6564b97f26bc08151fd53577e9e718e2af85e2
HuiminLiu <hihuimin@gmail.com>
2015-12-09 10:42:26 +0800

    relative path

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.engine('.html',ejs.__express);
 app.set('view engine', 'html'); 
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -26,6 +26,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
+app.use('/sign', routes);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
