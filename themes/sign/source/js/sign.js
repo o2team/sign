5c807b76ad848ab1d99118761055ca1c07ef5e69
Tingglelaoo <litingting6@jd.com>
2016-07-18 17:14:33 +0800

    1.增加配色设置功能 2.浏览器版本提醒 3.排版

--- themes/sign/source/js/sign.js
+++ themes/sign/source/js/sign.js
@@ -12,57 +12,88 @@ var sign = new Vue({
                 mobile: ''
             }
         },
+        style: {
+            logo: '',
+            co_title: '#000000',
+            co_item: '#999999',
+            co_icon: '#648fe7',
+            co_bg:'#eeeeee',
+            co_line: '#000050'
+        },
         cropper: null,
         flag: {
+            showBowserUnsupport: false,
             needLoadSign: false,
+            showColorSetting: false,
             imgHasLoad: false,
             cropperHasInit: false,
             previewHasCreate: false
         },
+        img: null,
         helper: {
             support: !!(window.FileReader && window.CanvasRenderingContext2D && (window.URL || window.webkitURL)),
             isImage: function(type) {
                 var filter = /^(?:image\/bmp|image\/cis\-cod|image\/gif|image\/ief|image\/jpeg|image\/jpeg|image\/jpeg|image\/pipeg|image\/png|image\/svg\+xml|image\/tiff|image\/x\-cmu\-raster|image\/x\-cmx|image\/x\-icon|image\/x\-portable\-anymap|image\/x\-portable\-bitmap|image\/x\-portable\-graymap|image\/x\-portable\-pixmap|image\/x\-rgb|image\/x\-xbitmap|image\/x\-xpixmap|image\/x\-xwindowdump)$/i;
                 return !!(filter.test(type));
-            },
-            each: function(arr, callback) {
-                var length = arr.length;
-                var i;
-                for (i = 0; i < length; i++) {
-                    callback.call(arr, arr[i], i, arr);
-                }
-                return arr;
             }
         }
     },
     ready: function() {
+        this.checkIfChrome();
         this.bindLoadImgEvent();
     },
     methods: {
-        bindLoadImgEvent: function() {
+        resetColors: function(){ // 重置配色
+            var thisObj = this;
+             thisObj.style = {
+                co_title: '#000000',
+                co_item: '#999999',
+                co_icon: '#648fe7',
+                co_bg:'#eeeeee',
+                co_line: '#000050'
+             }
+        },
+        checkIfChrome: function(){// 检测是否为chrome浏览器，如果不是，则不支持配色设置
+            var thisObj = this;
+            var reg_Chrome = /chrome\/[\d.]+/gi;
+            var bowser = navigator.userAgent.match(reg_Chrome);
+            if(bowser){
+                var ver = (bowser+'').replace(/[^0-9.]/ig,'').split('.')[0];
+                if(parseInt(ver,10) >= 49) {
+                    thisObj.flag.showBowserUnsupport = false;
+                    return ;
+                }
+            }
+            thisObj.flag.showBowserUnsupport = true;
+        },
+        bindLoadImgEvent: function() { //绑定监听事件
             var thisObj = this;
             if (!thisObj.helper.support) return;
             var uploadPreview = document.getElementById('uploadPreview');
-            var uploadImage = document.getElementById('uploadImage');
+            var uploadImages = document.querySelectorAll('.J_uploadImage');
             var reader = new FileReader();
             var URL = window.URL || window.webkitURL;
             var blobURL;
-            uploadImage.addEventListener('change', function() {
-                var files = this.files;
-                if (files.length === 0) return;
-                var file = files[0];
-                if (!thisObj.helper.isImage(file.type)) {
-                    alert('you have to select an image file!');
-                    return;
-                }
-                reader.readAsDataURL(file);
-                blobURL = URL.createObjectURL(file);
-                if (thisObj.cropper) {
-                    thisObj.cropper.reset();
-                }
-                thisObj.flag.imgHasLoad = true;
-                uploadImage.value = null;
-            });
+            var len = uploadImages.length;
+            for(var i =0;i<len;i++) {
+                uploadImages[i].addEventListener('change', function() {
+                    var files = this.files;
+                    if (files.length === 0) return;
+                    var file = files[0];
+                    if (!thisObj.helper.isImage(file.type)) {
+                        alert('you have to select an image file!');
+                        return;
+                    }
+                    reader.readAsDataURL(file);
+                    blobURL = URL.createObjectURL(file);
+                    if (thisObj.cropper) {
+                        thisObj.cropper.reset();
+                    }
+                    thisObj.flag.imgHasLoad = true;
+                    thisObj.img = this.dataset.dest;
+                });
+            }
+            
             reader.onload = function(e) {
                 uploadPreview.src = e.target.result;
                 if (!thisObj.flag.cropperHasInit) {
@@ -72,7 +103,7 @@ var sign = new Vue({
                 thisObj.cropper.replace(blobURL);
             }
         },
-        loadCropper: function() {
+        loadCropper: function() { //加载裁剪工具
             var thisObj = this;
             var image = document.querySelector('#cropperBox > img');
             var preview = document.getElementById('cropperRes');
@@ -101,21 +132,22 @@ var sign = new Vue({
             thisObj.cropper = new Cropper(image, option);
             thisObj.flag.cropperHasInit = true;
         },
-        finishCropImage: function() {
+        finishCropImage: function() {//完成裁剪，并输出裁剪结果
             var thisObj = this;
             var croppedCanvas = thisObj.cropper.getCroppedCanvas({
                 width: 200,
                 height: 200
             });
             var imgDataUrl = croppedCanvas.toDataURL();
-            document.querySelector('.o2_sign .img img').src = imgDataUrl;
+            var where = '.o2_sign .' + thisObj.img + ' img';
+            document.querySelector(where).src = imgDataUrl;
             thisObj.flag.imgHasLoad = false;
         },
-        downloadRes: function() {
+        downloadRes: function() {// 利用DomToImage库输出签名图
             var thisObj = this;
             var sign = document.getElementById('sign');
             var imgName = this.info.e_name; 
-            domtoimage.toBlob(sign).then(function(blob){
+            domtoimage.toBlob(sign).then(function(blob){ 
                 window.saveAs(blob,imgName + '.png');
             });
         }
@@ -125,7 +157,6 @@ var sign = new Vue({
 
 // filter
 Vue.filter('formatTelNo', function(value) {
-
     if (value) {
         if (value.indexOf('-') !== -1) return value;
         var arr = value.split('');
@@ -147,3 +178,6 @@ Vue.filter('formatOfficeNo', function(value) {
         return '0755-32995555-' + num;
     }
 });
+
+
+
